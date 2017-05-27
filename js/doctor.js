60b977c5f8031f764ad9e1211a619914db7ea4ee
Dan Jacob <dansamueljacob@gmail.com>
2017-05-27 15:58:00 -0700

    add listing for doctor images

--- js/doctor.js
+++ js/doctor.js
@@ -2,21 +2,21 @@ var apiKey = require('./../.env').apiKey;
 
 function Doctor(){
 }
-//#############API CALL################//
 Doctor.prototype.getDoctors = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey);
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
+  console.log(response);
   response.data.forEach(function(doctor){
     var first_name = doctor.profile.first_name;
     var last_name = doctor.profile.last_name;
     var img = doctor.profile.image_url;
     $("#findDoctor").append("<li>" + "First Name: " +  first_name + "</li>" + "<li>" + "Last Name " + last_name + "</li>" + "<br>"+ "<li>" + '</li><li> <img src="' + img + '" alt="No image available."</li><br><br>');
     });
-  });
+  })
 
 
    .fail(function(error){
       $("#findDoctor").text(error.responseJSON.message);
   });
-//#############API CALL################//
+};
 
 exports.doctorModule = Doctor;
