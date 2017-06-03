dd8a196401ea18f58f0dacc6a20b101893dfef7c
Dan Jacob <dansamueljacob@gmail.com>
2017-06-03 16:47:05 -0700

    separate front/back end logic

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,11 +1,23 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
 
+
+var displayDoctor = function(medicalIssue, response){
+  response.data.forEach(function(doctor){
+    var first_name = doctor.profile.first_name;
+    var last_name = doctor.profile.last_name;
+    var img = doctor.profile.image_url;
+    $("#findDoctor").append("<li>" + "First Name: " +  first_name + "</li>" + "<li>" + "Last Name " + last_name + "</li>" + "<br>"+ "<li>" + '</li><li> <img src="' + img + '" alt="No image available."</li><br><br>');
+    });
+
+};
+
+
 $(document).ready(function() {
-  $('#issue-form').submit(function() {
+  $('#issue-form').submit(function(event) {
     event.preventDefault();
     var newDoc = new Doctor();
     var medicalIssue = $('#issue').val();
-    newDoc.getDoctors(medicalIssue);
+    newDoc.getDoctors(medicalIssue, displayDoctor);
   });
 });
