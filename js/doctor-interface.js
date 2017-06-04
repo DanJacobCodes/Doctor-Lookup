ed55a0ab17fae3eea1f8739bf8d28173d4658f36
Dan Jacob <dansamueljacob@gmail.com>
2017-06-03 18:22:56 -0700

    update styles

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,12 +2,12 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 
 
 
-var displayDoctor = function(medicalIssue, response){
+var displayDoctor = function(response){
   response.data.forEach(function(doctor){
     var first_name = doctor.profile.first_name;
     var last_name = doctor.profile.last_name;
     var img = doctor.profile.image_url;
-    $("#findDoctor").append("<li>" + "First Name: " +  first_name + "</li>" + "<li>" + "Last Name " + last_name + "</li>" + "<br>"+ "<li>" + '</li><li> <img src="' + img + '" alt="No image available."</li><br><br>');
+    $("#findDoctor").append("<li>" + "First Name: " +  first_name + "</li>" + "<li>" + "Last Name: "+ last_name + "</li>" + "<br>" + "<li>" + '</li>  <li> <img src="' + img + '" alt="No image available."</li>');
     });
 
 };
--- js/doctor.js
+++ js/doctor.js
@@ -4,12 +4,12 @@ function Doctor(){
 }
 
 Doctor.prototype.getDoctors = function(medicalIssue, displayDoctor) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey, function(doctor){
-    displayDoctor(medicalIssue, doctor)
-  .then(function(response) {
-  });
-  return response;
-  });
-};
-
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+  .then(function(response){
+    displayDoctor(response)
+  })
+  .fail(function(error){
+    $('#findDoctor').text(error.responseJSON);
+    });
+  };
 exports.doctorModule = Doctor;
